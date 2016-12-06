9c98b7702a143de7805e9b6736158847cecbf1b6
Hasanain Jamal <hasanain.jamal1@aexp.com>
2016-12-06 12:22:59 -0700

    changing nav to use click handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+class NavItem{
+  name: String;
+  route: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,7 +12,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  navItems = [
+  navItems: NavItem[] = [
     {
       'name': 'Welecome',
       'route': '/welcome'
@@ -17,4 +23,10 @@ export class AppComponent {
     }
     ];
   title = 'Gift exchange';
+  constructor(private router: Router){
+   
+  }
+  onSelect(navItem: NavItem){
+      this.router.navigate([navItem.route])
+  }
 }
